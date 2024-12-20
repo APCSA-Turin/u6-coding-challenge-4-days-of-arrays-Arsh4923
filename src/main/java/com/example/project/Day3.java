36e3d397e1ec02783b4e609bbf28db9b385ad2c8
Arsh4923 <arshdeeps26@nycstudents.net>
2024-12-20 16:22:42 +0000

    Lebron

--- src/main/java/com/example/project/Day3.java
+++ src/main/java/com/example/project/Day3.java
@@ -1,20 +1,46 @@
 package com.example.project;
 
 public class Day3 {
-      public static String[][] generateSnowflake(int size) { //you will be tested on this method
+    public static String[][] generateSnowflake(int size) { // you will be tested on this method
         String[][] grid = new String[size][size];
+       
+        // Initialize the grid with spaces
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = " ";
+            }
+        }
 
-    
+        int center = size / 2;
+
+        // Place the stars for the snowflake design
+        for (int i = 0; i < size; i++) {
+            grid[i][center] = "*";  // Vertical line through the center
+            grid[center][i] = "*";  // Horizontal line through the center
+            if (i == center) continue; // Avoid overlap of center star
+
+            // Diagonal lines for symmetry
+            grid[i][i] = "*";  // Top-left to bottom-right diagonal
+            grid[i][size - i - 1] = "*";  // Top-right to bottom-left diagonal
+        }
 
         return grid;
     }
 
-    // Prints the snowflake  will be useful if tests fail (you will not be tested on this method)
+    // Prints the snowflake, will be useful if tests fail (you will not be tested on this method)
     public static void printSnowflake(String[][] snowflake) {
-        
+        for (String[] row : snowflake) {
+            for (String cell : row) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
     }
 
     // Test for the snowflake generation
     public static void main(String[] args) {
+        int size = 7;  // Example size
+        String[][] snowflake = generateSnowflake(size);
+        printSnowflake(snowflake);
     }
 }
--- src/main/java/com/example/project/Day4.java
+++ src/main/java/com/example/project/Day4.java
@@ -2,38 +2,85 @@ package com.example.project;
 import java.util.Random;
 
 public class Day4 {
-    
-    private static String[] reindeer_names = {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"};
+
+    private static String[] reindeerNames = {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"};
 
     public static class Reindeer { // you must have at least a name, distance travelled, speed, run duration, and rest duration attribute
-
+        private String name;
+        private int distanceTraveled;
+        private int speed;
+        private int runDuration;
+        private int restDuration;
+        private int timeRunning;
+        private int timeResting;
+        private boolean isRunning;
 
         // Constructor 
         public Reindeer(String name, int speed, int runDuration, int restDuration) {
+            this.name = name;
+            this.speed = speed;
+            this.runDuration = runDuration;
+            this.restDuration = restDuration;
+            this.distanceTraveled = 0;
+            this.timeRunning = 0;
+            this.timeResting = 0;
+            this.isRunning = true;
         }
 
-        public int getDistanceTraveled(){ // this method is required
-            return 0;
-        }
-        
-
-        public void simulateSecond(){ //this method is required but not tested
-            
+        public int getDistanceTraveled() { // this method is required
+            return distanceTraveled;
         }
 
+        public void simulateSecond() { // this method is required but not tested
+            if (isRunning) {
+                distanceTraveled += speed;
+                timeRunning++;
+                if (timeRunning == runDuration) {
+                    isRunning = false;
+                    timeRunning = 0;
+                }
+            } else {
+                timeResting++;
+                if (timeResting == restDuration) {
+                    isRunning = true;
+                    timeResting = 0;
+                }
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
-    public static String simulateRace(int time, Reindeer[] reindeers){ //you will be tested on this method
-        return "name of winner";
+    public static String simulateRace(int time, Reindeer[] reindeers) { // you will be tested on this method
+        for (int i = 0; i < time; i++) {
+            for (Reindeer reindeer : reindeers) {
+                reindeer.simulateSecond();
+            }
+        }
+
+        Reindeer winner = reindeers[0];
+        for (Reindeer reindeer : reindeers) {
+            if (reindeer.getDistanceTraveled() > winner.getDistanceTraveled()) {
+                winner = reindeer;
+            }
+        }
+        return winner.getName();
     }
 
-    
-    public static void main(String[] args) { // for testing purposed
+    public static void main(String[] args) { // for testing purposes
+        Random rand = new Random();
+        Reindeer[] reindeers = new Reindeer[reindeerNames.length];
 
+        for (int i = 0; i < reindeerNames.length; i++) {
+            int speed = rand.nextInt(10) + 5; // random speed between 5 and 15
+            int runDuration = rand.nextInt(5) + 5; // random run duration between 5 and 10 seconds
+            int restDuration = rand.nextInt(5) + 5; // random rest duration between 5 and 10 seconds
+            reindeers[i] = new Reindeer(reindeerNames[i], speed, runDuration, restDuration);
+        }
+
+        String winner = simulateRace(10, reindeers); // example with 10 seconds
+        System.out.println("Winner: " + winner);
     }
-
- 
 }
-
-
-
